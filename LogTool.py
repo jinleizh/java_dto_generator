7cb336bb72de26d0862c097b308d7ab2952d2122
ctrlzhang <344145863@qq.com>
2016-06-02 09:53:10 +0800

    1.更新log路径

--- LogTool.py
+++ LogTool.py
@@ -7,7 +7,7 @@ import logging.handlers
 logger = logging.getLogger("logger")
 log_level = logging.DEBUG
 logger.setLevel(log_level)
-log_file = "./CodeGenereator.log"
+log_file = "./log/CodeGenereator.log"
 log_handle = logging.handlers.TimedRotatingFileHandler(log_file, 'midnight', 1, 30)
 log_handle.suffix = "%Y%m%d.log"
 log_format = logging.Formatter('%(asctime)s|%(filename)s:%(lineno)d|%(funcName)s|%(levelname)s|%(message)s')
@@ -19,7 +19,7 @@ logger.addHandler(log_handle)
 report_logger = logging.getLogger("report_logger")
 report_log_level = logging.DEBUG
 report_logger.setLevel(report_log_level)
-report_log_file = "./report.log"
+report_log_file = "./log/report.log"
 report_log_handle = logging.handlers.TimedRotatingFileHandler(report_log_file, 'midnight', 1, 30)
 report_log_handle.suffix = "%Y%m%d.log"
 report_log_handle.setFormatter(log_format)
